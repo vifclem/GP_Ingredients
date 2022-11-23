b343d39c18037ea3b0faed35815d6b5af0b4b2b8
vifclem <clement.expert@artfx.fr>
2022-11-23 15:18:24 +0100

    [add]
    chest, key anim

--- Assets/Scripts/Marie/Items/Interactive/WallTorch.cs
+++ Assets/Scripts/Marie/Items/Interactive/WallTorch.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class WallTorch : Interactive
 {
+
+    public static int litTorchesCount = 0;
+    public GameObject chest;
+
     public override void OnInteraction()
     {
         //If I want to do the base OnInteraction anyway first
@@ -11,5 +15,11 @@ public class WallTorch : Interactive
         //Activate light and fire
         transform.GetChild(0).gameObject.SetActive(true);
         transform.GetChild(1).gameObject.SetActive(true);
+        litTorchesCount++;
+        if(litTorchesCount == 3)
+        {
+            //spawn chest
+            chest.SetActive(true);
+        }
     }
 }
